e104af70f1f6e1b71a0962293e44f0187ef749e9
Rayilla Tarun <rayillatarun@gmail.com>
2021-12-20 11:40:18 +0530

    initial commit

---
+++ app/src/main/java/com/example/githubtest/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.githubtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //some code
+    }
+}
